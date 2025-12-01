be97a57d85b2c14b524dce085114ed927f6f78e2
ZeinebMokni <zeinebmokni@gmail.com>
2025-12-01 20:53:29 +0100

    Remove backoffice from main branch

--- views/users/delete.php
+++
@@ -1,7 +0,0 @@
-<h2>Delete User</h2>
-<p>Are you sure?</p>
-
-<a href="index.php?action=users" class="btn btn-secondary">Cancel</a>
-<form method="POST" action="index.php?action=user_destroy&id=<?= $id ?>" style="display:inline;">
-    <button class="btn btn-danger">Delete</button>
-</form>
